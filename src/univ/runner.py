a55eac48936766c4278a2cb6587714fc6b506abb
hyeonjulee <dlguswn77771@naver.com>
2020-03-17 16:22:08 +0900

    bug fixed

--- src/univ/runner.py
+++ src/univ/runner.py
@@ -4,11 +4,10 @@ import time
 import pandas as pd
 from bs4 import BeautifulSoup
 
-df = pd.read_csv("./univ/base.csv")
-
 
 def kangwon():
-    # 강원대학교 추천도서는 20200316 기준으로 155페이지
+    # 강원대학교 추천도서는 5권씩 155페이지 (20200316)
+    df = pd.read_csv("./univ/base.csv")
     df_index = 0
     for page in range(1, 156):
         res = requests.get(f'http://lib.kangwon.ac.kr:8080/board/list_book.jsp?pg={page}&bcs=60&re=1')
@@ -21,12 +20,14 @@ def kangwon():
                                 line.split("   ")[3].split(":")[1].strip()]
             df_index += 1
         time.sleep(0.5)
+        print(f'kangwon {df_index}')
     df.to_csv("../output/kangwon.csv", encoding="utf-8-sig", index=False)
     print("crawlbooks/output/kangwon.csv is saved")
 
 
 def gyeongsang():
-    # 경상대학교 교양권장 100선은 10권씩 10페이지
+    # 경상대학교 교양권장 100선은 10권씩 10페이지 (20200317)
+    df = pd.read_csv("./univ/base.csv")
     df_index = 0
     for page in range(1, 11):
         res = requests.get(f'http://books.gnu.ac.kr/local/recommend/recommend100List.do?pageNo={page}')
@@ -44,5 +45,6 @@ def gyeongsang():
             df.loc[df_index] = [title, author, publisher]
             df_index += 1
         time.sleep(0.5)
+        print(f'gyeongsang {df_index}')
     df.to_csv("../output/gyeongsang.csv", encoding="utf-8-sig", index=False)
     print("crawlbooks/output/gyeongsang.csv is saved")
